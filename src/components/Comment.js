7b2841d5497f7f76b7f628ea41299bc7e0c18e86
kna <kochmarev.n.a@gmail.com>
2016-11-21 16:10:00 +0300

    : comment functional component stateless

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react'
 //Здесь явно лишний стейт. Убери его и перепиши компонент на Functional Component
-class Comment extends Component {
-
-    constructor() {
-        super()
-        this.state = {
-            isOpen: false
-        }
-    }
-
-    render() {
+// готово
+function Comment(props) {
         return (
           <span>
-            <b> {this.props.comment.user} </b>
-            <p> {this.props.comment.text} </p>
+            <b> {props.comment.user} </b>
+            <p> {props.comment.text} </p>
             </span>
-        )
-    }
-
-    handleClick = ev => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+        );
 }
 
 export default Comment
