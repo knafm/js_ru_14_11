dd8a9f62d2861a41fbd73b5238802233e474468d
Nikita Kochmarev <kochmarev.n.a@gmail.com>
2016-11-27 19:35:32 +0400

    calendar ht3.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,42 @@
-import React, { Component, PropTypes } from 'react'
+import React, {Component, PropTypes} from 'react'
 import ArticleList from './ArticleList'
 import Select from 'react-select'
 import Chart from './Chart'
 import 'react-select/dist/react-select.css'
+import DayPicker, {DateUtils} from "react-day-picker";
+import 'react-day-picker/lib/style.css'
+import moment from 'moment';
+
 
 class App extends Component {
     static propTypes = {
         articles: PropTypes.array.isRequired
     };
 
+    constructor(props) {
+        super(props);
+        this.handleDayClick = this.handleDayClick.bind(this);
+        this.handleResetClick = this.handleResetClick.bind(this);
+    }
+
     state = {
-        selected: null
+        selected: null,
+        selectedDay: new Date(),
+        from: null,
+        to: null,
+    }
+
+    handleDayClick(e, day) {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    }
+
+    handleResetClick(e ) {
+        e.preventDefault();
+        this.setState({
+            from: null,
+            to: null,
+        });
     }
 
     render() {
@@ -18,16 +44,26 @@ class App extends Component {
             label: article.title,
             value: article.id
         }))
+        const { from, to } = this.state;
         return (
             <div>
                 <Chart />
-                <ArticleList articles={this.props.articles} />
-                <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi = {true}/>
+                <ArticleList articles={this.props.articles}/>
+                <Select options={options} value={this.state.selected} onChange={this.handleChange} multi={true}/>
+                <DayPicker
+                    initialMonth={ new Date(2016, 1) }
+                    selectedDays={ day => DateUtils.isDayInRange(day, {from, to}) }
+                    onDayClick={ this.handleDayClick.bind(this) }
+                />
+                <p>
+                    Диапазон: { moment(from).format('L') } - { moment(to).format('L') }.
+                    { ' ' }<a href="." onClick={ this.handleResetClick }>Reset</a>
+                </p>
             </div>
         )
     }
 
-    handleChange = selected => this.setState({ selected })
+    handleChange = selected => this.setState({selected})
 }
 
 export default App
