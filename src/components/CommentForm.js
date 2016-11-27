9bd6618d6f1d5702327ad52ee1fad2f846421fe8
Nikita Kochmarev <kochmarev.n.a@gmail.com>
2016-11-27 19:58:50 +0400

    error

---
+++ src/components/CommentForm.js
@@ -0,0 +1,15 @@
+/**
+ * Created by odd on 27.11.2016.
+ */
+import React, { PropTypes } from 'react'
+
+function CommentForm(props) {
+
+    return (
+        <div>
+            куку
+        </div>
+    )
+}
+
+export default CommentForm
