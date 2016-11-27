adda6436324efb07272625b5f8e0ca79fb5bcd41
Nikita Kochmarev <kochmarev.n.a@gmail.com>
2016-11-27 21:28:23 +0400

    comment form

--- src/components/Commentform.js
+++ src/components/Commentform.js
@@ -1,29 +1,29 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 
 class CommentForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
 
         this.handleChange = this.handleChange.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    state = {
-        value:'',
     }
+
+    state = {
+        value: '',
+    }
+
     handleChange(event) {
         this.setState({value: event.target.value});
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <form>
                     комментарий:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <input type="button" value="Submit" />
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <input type="button" value="Submit"/>
                 </form>
-             </div>)
+            </div>)
     }
 }
 
