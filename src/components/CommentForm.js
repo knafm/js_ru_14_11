91648b07ccde79f5b2920eb2d2be337668d1c988
Nikita Kochmarev <kochmarev.n.a@gmail.com>
2016-11-27 21:09:32 +0400

    comment form

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,15 +1,30 @@
-/**
- * Created by odd on 27.11.2016.
- */
 import React, { PropTypes } from 'react'
 
-function CommentForm(props) {
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-    return (
-        <div>
-            куку
-        </div>
-    )
+        this.handleChange = this.handleChange.bind(this);
+        //this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    state = {
+        value:'',
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render(){
+        return(
+            <div>
+                <form>
+                    комментарий:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="button" value="Submit" />
+                </form>
+             </div>)
+    }
 }
 
 export default CommentForm
