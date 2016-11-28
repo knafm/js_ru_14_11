f4667e2040f5d1b27aa852ba72bcf644a307d42e
Roman <kaboom.ua@gmail.com>
2016-11-28 11:07:57 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import DayPicker, {DateUtils} from "react-day-picker";
 import 'react-day-picker/lib/style.css'
 import moment from 'moment';
 
-
+//лучше вынести DatePicker в отдельный компонент
 class App extends Component {
     static propTypes = {
         articles: PropTypes.array.isRequired
