eeeef680df80bae129ca190d887bbd16f1af33ce
Roman <kaboom.ua@gmail.com>
2016-12-07 22:22:09 +0200

    Update generator.js

--- src/middlewares/generator.js
+++ src/middlewares/generator.js
@@ -1,7 +1,9 @@
 import {ADD_COMMENT} from '../constants'
 import {updateArticleComments} from '../AC/articles'
 export default store => next => action => {
+      //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
     if (action.type === ADD_COMMENT){
+        //лучше не мутировать объект
         action.payload.id = new Date().getTime();
     }
     next(action);
